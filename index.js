3be26bb7d0d97f1bd574ef5aef11c53717fb3704
Siegfred Gamboa <siegy.gamboa@gmail.com>
2022-11-18 08:44:46 +0200

    configure vercel for deployment

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const cors = require('cors')
+const express = require('express')
+const mongoose = require('mongoose')
+const mongoString = process.env.DATABASE_URL
+const routes = require('./routes/routes')
+
+mongoose.connect(
+  process.env.DATABASE_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    console.log('connected')
+  }
+)
+
+const database = mongoose.connection
+const app = express()
+
+database.on('error', (error) => {
+  console.log(error)
+})
+
+database.once('connected', () => {
+  console.log('Database Connected')
+})
+app.use(cors())
+app.use(express.json())
+app.use('/', routes)
+
+app.listen(process.env.PORT || 5001, (err) => {
+  if (err) throw err
+  console.log('Server started')
+})
---
+++ model/model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const dataSchema = new mongoose.Schema({
+  title: {
+    required: true,
+    type: String,
+  },
+  briefDesc: {
+    required: true,
+    type: String,
+  },
+  resultDesc: {
+    required: true,
+    type: String,
+  },
+  github: {
+    required: true,
+    type: String,
+  },
+  demoLink: {
+    required: true,
+    type: String,
+  },
+  tech: {
+    required: true,
+    type: String,
+  },
+  image: [
+    {
+      required: true,
+      type: String,
+    },
+  ],
+})
+
+module.exports = mongoose.model('Data', dataSchema)
